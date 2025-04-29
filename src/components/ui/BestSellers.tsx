e5ab9e74d055f0a16c7a13107c63a1a9bab86f9d
ihza6661 <ihzahmahendra6661@gmail.com>
2025-04-29 15:10:12 +0700

    ,

--- src/components/ui/BestSellers.tsx
+++ src/components/ui/BestSellers.tsx
@@ -64,7 +64,7 @@ const ProductCard = ({ product }: { product: Product }) => {
       <div className="aspect-[3/4] overflow-hidden">
         <Link to={`/product/${product.id}`}>
           <img
-            src={product.image}
+            src={product.images[0]}
             alt={product.name}
             className="w-full h-auto object-cover hover:scale-105 transition-transform duration-500"
           />
